11ef68be1694b9df2e048ed3456042fb6a36000e
Michael Chen <mchen7588@gmail.com>
2018-12-03 08:39:06 -0800

    add play interaction for not won or not lost game state, stub out continue game flow

--- text_client/lib/text_client/player.ex
+++ text_client/lib/text_client/player.ex
@@ -3,12 +3,59 @@ defmodule TextClient.Player do
 
     # possible states:
     # initializing, won, lost, good_guess, bad_guess, invalid_move, already_used
-    def play(%State{ tally: { game_state: :won } }) do
+
+    def play(game = %State{ tally: %{ game_state: :good_guess } }) do
+        continue_with_message(game, "good guess")
+    end
+
+    def play(game = %State{ tally: %{ game_state: :bad_guess } }) do
+        continue_with_message(game, "bad guess")
+    end
+
+    def play(game = %State{ tally: %{ game_state: :invalid_move } }) do
+        continue_with_message(game, "invalid move")
+    end
+
+    def play(game = %State{ tally: %{ game_state: :already_used } }) do
+        continue_with_message(game, "already used")
+    end
+    
+    def play(%State{ tally: %{ game_state: :won } }) do
         exit_with_message("you won")
     end
     
-    def play(%State{ tally: { game_state: :lost } }) do
+    def play(%State{ tally: %{ game_state: :lost } }) do
         exit_with_message("you lost")
+    end
+
+    def play(game) do
+        continue(game)
+    end
+
+    defp continue_with_message(game, message) do
+        IO.puts(message)
+
+        continue(game)
+    end
+
+    defp continue(game) do
+        game
+            |> display()
+            |> prompt()
+            |> make_move()
+            |> play()
+    end
+
+    defp display(game) do
+        game
+    end
+
+    defp prompt(game) do
+        game
+    end
+
+    defp make_move(game) do
+        game
     end
     
     defp exit_with_message(message) do
